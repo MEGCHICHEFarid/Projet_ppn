ded50c7704990ccca3a351096e33cf92be32a529
Farid Megchiche <megchiche.farid@gmail.com>
2021-01-22 01:58:20 +0100

    appel aux autres ligne de l'algorithme1

--- algo1.py
+++ algo1.py
@@ -4,3 +4,19 @@ print("k l'ordre de dégénérescence")
 from ligne1 import test1
 
 
+print ("liste d'adjacence dégénérée")
+
+from ligne2 import Graph
+
+
+print("sous graphe de G")
+
+from sous_graphe_G import sousgraph
+
+
+print("Cliques maximales")
+
+from bronkerbosh import BronKerbosch2
+
+
+
