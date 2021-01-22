00e0d9e0310106f72f2b147ff9a49e8bdca93d74
Farid Megchiche <megchiche.farid@gmail.com>
2021-01-22 02:15:05 +0100

    algorithme2: appel au meme lignes implementées dans l'algorithme1: meme ligne1->4)

---
+++ algo2.py
@@ -0,0 +1,26 @@
+print ("----------algorithme 2----------------")
+
+print("k l'ordre de dégénérescence")
+
+from ligne1 import test1
+
+print ("------------------------------------")
+
+print ("liste d'adjacence dégénérée")
+
+from ligne2 import Graph
+
+print ("--------------------------------------")
+
+print("sous graphe de G")
+
+from sous_graphe_G import sousgraph
+
+print ("--------------------------------------")
+
+print("Cliques maximales")
+
+from bronkerbosh import BronKerbosch2
+
+
+
