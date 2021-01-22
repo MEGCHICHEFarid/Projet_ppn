2d842718eff2bdea62735e2b7074a615f49ea452
Farid Megchiche <megchiche.farid@gmail.com>
2021-01-22 02:05:16 +0100

    modification de l'ergonomie de l'affichage

--- algo1.py
+++ algo1.py
@@ -1,18 +1,22 @@
+print ("----------algorithme 1----------------")
 
 print("k l'ordre de dégénérescence")
 
 from ligne1 import test1
 
+print ("------------------------------------")
 
 print ("liste d'adjacence dégénérée")
 
 from ligne2 import Graph
 
+print ("--------------------------------------")
 
 print("sous graphe de G")
 
 from sous_graphe_G import sousgraph
 
+print ("--------------------------------------")
 
 print("Cliques maximales")
 
